f632e6cb9457fe70bf3f5fb834b23205bd15c6c2
Florian <florian.sasso@outlook.fr>
2023-01-26 20:55:57 +0100

    Rename variables - crypto

--- lib/scrapping_bitcoin.rb
+++ lib/scrapping_bitcoin.rb
@@ -7,7 +7,16 @@ require 'open-uri'
 def scraper(url)
   # Open and parse a url
   page = parsed_page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+# Creating a name list
 
+name_currency = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--hide-sm cmc-table__cell--sort-by__symbol"]')
+name_currency_array = []
+
+  name_currency.each do |element|
+    name_currency_array << element.text
+  end
+  # puts name_currency_array
+  
   # Creating a price list
 
   price_currency = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"]')
@@ -17,21 +26,13 @@ def scraper(url)
       price_currency_array << element.text
     end
     # puts price_currency_array
-  # Creating a name list
-
-  name_currency = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--hide-sm cmc-table__cell--sort-by__symbol"]')
-  name_currency_array = []
-
-    name_currency.each do |element|
-      name_currency_array << element.text
-    end
-    # puts name_currency_array
+  
 
   # Hash 2 lists
   
   name_price = Hash[name_currency_array.zip(price_currency_array)]
   puts name_price.inspect
-  
+
 end
 url = "https://coinmarketcap.com/all/views/all/"
 scraper(url)
